959c5508b5d886b6bc8a0c2ab315ea7ff60c4343
alexsharich <alexsharich@gmail.com>
2021-12-16 23:19:38 +0100

    add HW11 SuperRange

--- src/p2-homeworks/h11/HW11.tsx
+++ src/p2-homeworks/h11/HW11.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import SuperRange from './common/c7-SuperRange/SuperRange'
 import SuperDoubleRange from './common/c8-SuperDoubleRange/SuperDoubleRange'
 
@@ -6,32 +6,36 @@ function HW11() {
     const [value1, setValue1] = useState(0)
     const [value2, setValue2] = useState(100)
 
-    return (
-        <div>
-            <hr/>
-            homeworks 11
+    const onChangeRange = (value: number) => {
+        setValue1(value)
+    }
 
-            {/*should work (должно работать)*/}
+    return (
+        <div className='HWBlock'>
+
+            <h3>homeworks 11</h3>
+
+
             <div>
                 <span>{value1}</span>
                 <SuperRange
-                    // сделать так чтоб value1 изменялось
+                    value={value1} onChangeRange={onChangeRange}
                 />
             </div>
 
             <div>
                 <span>{value1}</span>
                 <SuperDoubleRange
-                    // сделать так чтоб value1 и value2 изменялось
+                // сделать так чтоб value1 и value2 изменялось
                 />
                 <span>{value2}</span>
             </div>
 
-            <hr/>
+            <hr />
             {/*для личного творчества, могу проверить*/}
             {/*<AlternativeSuperRange/>*/}
             {/*<AlternativeSuperDoubleRange/>*/}
-            <hr/>
+            <hr />
         </div>
     )
 }
--- src/p2-homeworks/h11/common/c7-SuperRange/SuperRange.tsx
+++ src/p2-homeworks/h11/common/c7-SuperRange/SuperRange.tsx
@@ -1,4 +1,4 @@
-import React, {ChangeEvent, DetailedHTMLProps, InputHTMLAttributes} from 'react'
+import React, { ChangeEvent, DetailedHTMLProps, InputHTMLAttributes } from 'react'
 import s from './SuperRange.module.css'
 
 // тип пропсов обычного инпута
@@ -33,7 +33,7 @@ const SuperRange: React.FC<SuperRangePropsType> = (
                 type={'range'}
                 onChange={onChangeCallback}
                 className={finalRangeClassName}
-
+                value={restProps.value}
                 {...restProps} // отдаём инпуту остальные пропсы если они есть (value например там внутри)
             />
         </>
